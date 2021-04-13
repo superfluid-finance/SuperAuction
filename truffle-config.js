8761e52768c8724b05f7a346d86d819da6f1c34e
Axe <ngsmo@iscte-iul.pt>
2021-04-13 18:21:11 +0300

    update goerli gas price

--- truffle-config.js
+++ truffle-config.js
@@ -56,7 +56,7 @@ module.exports = {
       ),
       network_id: 5, // Goerli's id
       gas: 8e6,
-      gasPrice: +process.env.GOERLI_GAS_PRICE || 2e9, // 100 GWEI, goerli is busy!
+      gasPrice: +process.env.GOERLI_GAS_PRICE || 10e9, // 100 GWEI, goerli is busy!
       confirmations: 6, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
@@ -144,7 +144,7 @@ module.exports = {
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: false,
+          enabled: true,
           runs: 200
         },
       //  evmVersion: "byzantium"
